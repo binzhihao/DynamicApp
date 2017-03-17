96010d9d833529486e6c81767b0816c9dfb48daf
Zhihao Bin <Zhihao Bin>
2017-03-17 17:33:21 +0800

    change icon && app name

--- app/src/main/java/io/bean/android/knock/common/Constants.java
+++ app/src/main/java/io/bean/android/knock/common/Constants.java
@@ -1,14 +1,17 @@
 package io.bean.android.knock.common;
 
-import android.content.Context;
-import android.os.Environment;
-
-import java.io.File;
-
 public class Constants {
     public static final boolean DEBUG = true;
-    public static final String CONFIG = "knock";
-    public static final String MD5 = "1338CAF669D209FB2C6B9F16F5AF905C";
-    public static final String TEMP = "6b46cd72";
+    public static final String CONFIG = "dynamic";
+    //public static final String MD5 = "1338CAF669D209FB2C6B9F16F5AF905C";
+    //public static final String TEMP = "6b46cd72";
+    public static final String TEMP = "6b46cd73";
+    public static final String MD5 = "2B866A6D0F826D03FD2E7DF2E7551488";
+    //public static final String TEMP = "6b46cd74";
+    //public static final String MD5 = "FD28FC83E8CECB5008028D288225B89C";
+    //public static final String TEMP = "6b46cd75";
+    //public static final String MD5 = "7B63C52103AD0E1FA0EED7C16F500A56";
+    //public static final String TEMP = "6b46cd76";
+    //public static final String MD5 = "F6BE69DC7A0BBF743C7ED4FCC062A844";
     public static final long DELAY = 2000;
 }
