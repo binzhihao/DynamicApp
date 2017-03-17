79ea362d0422ea46981dbc95e15eb1b9791851ee
Zhihao Bin <Zhihao Bin>
2017-03-17 10:40:40 +0800

    adapt for android n

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DynamicApp
-A framework to load app dynamically.
+A framework to load app dynamically. Please abide by the GPL agreement, otherwise you will be investigated for legal responsibility!
--- app/src/main/java/io/bean/android/knock/App.java
+++ app/src/main/java/io/bean/android/knock/App.java
@@ -2,10 +2,18 @@ package io.bean.android.knock;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 
+import com.lody.virtual.client.core.InstallStrategy;
 import com.lody.virtual.client.core.VirtualCore;
+import com.lody.virtual.helper.proto.InstallResult;
 
 public class App extends Application {
+
+    private static final String[] GMS_PKG = {
+            "com.google.android.gms",
+    };
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -20,6 +28,26 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (VirtualCore.get().isMainProcess()) {
+            // Install the Google mobile service
+            installGms();
+        }
     }
 
+    private void installGms() {
+        PackageManager pm = VirtualCore.get().getUnHookPackageManager();
+        for (String pkg : GMS_PKG) {
+            try {
+                ApplicationInfo appInfo = pm.getApplicationInfo(pkg, 0);
+                String apkPath = appInfo.sourceDir;
+                InstallResult res = VirtualCore.get().installApp(apkPath,
+                        InstallStrategy.DEPEND_SYSTEM_IF_EXIST | InstallStrategy.TERMINATE_IF_EXIST);
+                if (!res.isSuccess) {
+                    Utils.log("fuck", "Unable to install google app");
+                }
+            } catch (Throwable e) {
+                // Ignore
+            }
+        }
+    }
 }
